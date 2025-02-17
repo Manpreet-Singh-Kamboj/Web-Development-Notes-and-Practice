1ee0851a3f3239276e64889f82c0f32c004c171d
Manpreet Singh <97432223+Manpreet3033@users.noreply.github.com>
2025-02-17 22:04:30 +0530

    Learned basics of Javascript

---
+++ Live Cohort/Day_13_Starting_With_Javascript/script.js
@@ -0,0 +1,68 @@
+console.log("Hello world!");
+console.warn("This is a warning message.");
+console.error("This is a error message.");
+
+// Variables
+var a = 100;
+
+// Declaration
+var b;
+// Initialization
+b = 200;
+// Logging the Variables in Console
+console.log(a, b);
+// Reinitialization or Updating of Value
+b = 500;
+console.log(a, b);
+
+// Arithmetic Operations
+a = 10;
+b = 5;
+// Add
+console.log(a + b);
+// Subtract
+console.log(a - b);
+// Multiply
+console.log(a * b);
+// Division get the Quotient
+console.log(a / b);
+// Modulo get the Remainder
+console.log(a % b);
+
+var firstName = "Manpreet";
+var lastName = "Singh";
+// Concatenation
+console.log("Full Name: ", firstName, lastName);
+
+// Alert in Javascript
+// alert("Are you sure you want to continue?");
+
+// Confirm in Javascript
+// var ans = confirm("Are you 18+?");
+// console.log(ans);
+
+// Prompt in Javascript
+// var username = prompt("Please enter your full name: ");
+// console.log(username);
+
+a = 10;
+console.log(a++);
+console.log(a);
+
+var c = 10;
+console.log(c);
+
+let d = 30;
+console.log(d);
+d = 50;
+console.log(d);
+
+const constant = 100;
+console.log(constant);
+// constant = 120;
+
+// Difference between Var and Let
+if (10 < 12) {
+  let variable = 20;
+}
+console.log(variable);
