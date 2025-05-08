cfbb532b025d6a19706e077caa60ffca30e8e9e3
Manpreet Singh <manpreetkamboj60.net@gmail.com>
2025-05-08 22:19:48 +0530

    Todo List Features Added

--- Projects/DOM_Project/script.js
+++ Projects/DOM_Project/script.js
@@ -1,3 +1,9 @@
+const addTaskForm = document.querySelector(".addTask form");
+const addTaskFormInput = addTaskForm.querySelector("input");
+const addTaskFormTextArea = addTaskForm.querySelector("textarea");
+const addTaskMarkAsImportant = addTaskForm.querySelector("#imp");
+const allTasksContainer = document.querySelector(".allTasks");
+
 function openSelectedFeature() {
   const allElems = document.querySelectorAll(".elem");
   const fullElemPages = document.querySelectorAll(".fullElem");
@@ -5,7 +11,9 @@ function openSelectedFeature() {
 
   allElems.forEach((elem) => {
     elem.addEventListener("click", () => {
-      fullElemPages[elem.id].style.display = "block";
+      setTimeout(() => {
+        fullElemPages[elem.id].style.display = "block";
+      }, 200);
     });
   });
 
@@ -16,4 +24,83 @@ function openSelectedFeature() {
   });
 }
 
+function deleteTask(index) {
+  const tasks = JSON.parse(localStorage.getItem("tasks"));
+  tasks.splice(index, 1);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  renderTasks();
+}
+
+function renderTasks() {
+  const tasks = JSON.parse(localStorage.getItem("tasks"));
+  if (tasks.length === 0) {
+    allTasksContainer.innerHTML = `
+      <div class="no-tasks">
+        <p>No tasks available.</p>
+      </div>
+    `;
+    return;
+  }
+  let html = "";
+  tasks.forEach((task, index) => {
+    html += `
+    <div class="task" id="${index}">
+      <div class="content">
+        <h4>${task.title} <span class=${task.isImportant}>IMP</span></h4>
+        <p>
+          ${task.description}
+        </p>
+      </div>
+      <div class="actions">
+          <button class="delete" id="${index}">Mark as Completed</button>
+      </div>
+    </div>
+  `;
+  });
+  allTasksContainer.innerHTML = html;
+  const deleteTaskButtons = document.querySelectorAll(
+    ".allTasks .task .actions .delete"
+  );
+  deleteTaskButtons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      deleteTask(btn.id);
+    });
+  });
+}
+
+function addTask(task) {
+  const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  tasks.push(task);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  renderTasks();
+}
+
+function createTask() {
+  const taskTitle = addTaskFormInput.value;
+  const taskDescription = addTaskFormTextArea.value;
+  const isImportant = addTaskMarkAsImportant.checked;
+  if (!taskTitle || !taskDescription) {
+    alert("Please fill all the fields");
+    return;
+  }
+  const task = {
+    title: taskTitle,
+    description: taskDescription,
+    isImportant,
+  };
+  addTask(task);
+  addTaskFormInput.value = "";
+  addTaskFormTextArea.value = "";
+  addTaskMarkAsImportant.checked = false;
+}
+
+function todoListFeatures() {
+  addTaskForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    createTask();
+  });
+  renderTasks();
+}
+
 openSelectedFeature();
+todoListFeatures();
