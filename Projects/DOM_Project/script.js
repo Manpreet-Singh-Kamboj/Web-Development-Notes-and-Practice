f13dd872615052e1e7e78615150d8af828901270
Manpreet Singh <manpreetkamboj60.net@gmail.com>
2025-05-09 04:41:54 +0530

    Working on Daily Planner

--- Projects/DOM_Project/script.js
+++ Projects/DOM_Project/script.js
@@ -24,77 +24,76 @@ function openSelectedFeature() {
   });
 }
 
-function deleteTask(index) {
-  const tasks = JSON.parse(localStorage.getItem("tasks"));
-  tasks.splice(index, 1);
-  localStorage.setItem("tasks", JSON.stringify(tasks));
-  renderTasks();
-}
+function todoList() {
+  function deleteTask(index) {
+    const tasks = JSON.parse(localStorage.getItem("tasks"));
+    tasks.splice(index, 1);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    renderTasks();
+  }
 
-function renderTasks() {
-  const tasks = JSON.parse(localStorage.getItem("tasks"));
-  if (tasks.length === 0) {
-    allTasksContainer.innerHTML = `
-      <div class="no-tasks">
-        <p>No tasks available.</p>
+  function renderTasks() {
+    const tasks = JSON.parse(localStorage.getItem("tasks"));
+    if (tasks.length === 0) {
+      allTasksContainer.innerHTML = `
+        <div class="no-tasks">
+          <p>No tasks available.</p>
+        </div>
+      `;
+      return;
+    }
+    let html = "";
+    tasks.forEach((task, index) => {
+      html += `
+      <div class="task" id="${index}">
+        <div class="content">
+          <h4>${task.title} <span class=${task.isImportant}>IMP</span></h4>
+          <p>
+            ${task.description}
+          </p>
+        </div>
+        <div class="actions">
+            <button class="delete" id="${index}">Mark as Completed</button>
+        </div>
       </div>
     `;
-    return;
-  }
-  let html = "";
-  tasks.forEach((task, index) => {
-    html += `
-    <div class="task" id="${index}">
-      <div class="content">
-        <h4>${task.title} <span class=${task.isImportant}>IMP</span></h4>
-        <p>
-          ${task.description}
-        </p>
-      </div>
-      <div class="actions">
-          <button class="delete" id="${index}">Mark as Completed</button>
-      </div>
-    </div>
-  `;
-  });
-  allTasksContainer.innerHTML = html;
-  const deleteTaskButtons = document.querySelectorAll(
-    ".allTasks .task .actions .delete"
-  );
-  deleteTaskButtons.forEach((btn) => {
-    btn.addEventListener("click", () => {
-      deleteTask(btn.id);
     });
-  });
-}
-
-function addTask(task) {
-  const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
-  tasks.push(task);
-  localStorage.setItem("tasks", JSON.stringify(tasks));
-  renderTasks();
-}
-
-function createTask() {
-  const taskTitle = addTaskFormInput.value;
-  const taskDescription = addTaskFormTextArea.value;
-  const isImportant = addTaskMarkAsImportant.checked;
-  if (!taskTitle || !taskDescription) {
-    alert("Please fill all the fields");
-    return;
+    allTasksContainer.innerHTML = html;
+    const deleteTaskButtons = document.querySelectorAll(
+      ".allTasks .task .actions .delete"
+    );
+    deleteTaskButtons.forEach((btn) => {
+      btn.addEventListener("click", () => {
+        deleteTask(btn.id);
+      });
+    });
   }
-  const task = {
-    title: taskTitle,
-    description: taskDescription,
-    isImportant,
-  };
-  addTask(task);
-  addTaskFormInput.value = "";
-  addTaskFormTextArea.value = "";
-  addTaskMarkAsImportant.checked = false;
-}
 
-function todoListFeatures() {
+  function addTask(task) {
+    const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    tasks.push(task);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    renderTasks();
+  }
+
+  function createTask() {
+    const taskTitle = addTaskFormInput.value;
+    const taskDescription = addTaskFormTextArea.value;
+    const isImportant = addTaskMarkAsImportant.checked;
+    if (!taskTitle || !taskDescription) {
+      alert("Please fill all the fields");
+      return;
+    }
+    const task = {
+      title: taskTitle,
+      description: taskDescription,
+      isImportant,
+    };
+    addTask(task);
+    addTaskFormInput.value = "";
+    addTaskFormTextArea.value = "";
+    addTaskMarkAsImportant.checked = false;
+  }
   addTaskForm.addEventListener("submit", (e) => {
     e.preventDefault();
     createTask();
@@ -102,5 +101,8 @@ function todoListFeatures() {
   renderTasks();
 }
 
+function dailyPlanner() {}
+
 openSelectedFeature();
-todoListFeatures();
+todoList();
+dailyPlanner();
